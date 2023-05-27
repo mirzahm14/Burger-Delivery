b3fb9ac324cc6eb74cc9379717e97e6b34880e4c
Mirza Hafiz Muhammad <88041796+mirzahm14@users.noreply.github.com>
2023-05-27 16:41:28 +0700

    Add files via upload

---
+++ Project/Burger.java
@@ -0,0 +1,15 @@
+class Burger
+{
+    String name;                    // stores name of the pizza
+    int cost_L, cost_M, cost_S;     // variables to store the costs of large, medium and small size pizza
+    int time_reqd;                  // stores the time required to make the pizza
+
+    public Burger(String n, int cl, int cm, int cs, int t)      // parameterized constructor
+    {
+        name=n;
+        cost_L=cl;
+        cost_S= cs;
+        cost_M= cm;
+        time_reqd=t;
+    }
+}
